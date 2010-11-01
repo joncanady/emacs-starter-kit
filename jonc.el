4f56b0b1f5a8d5274b1eeea708e308fd2598a4be
Jon Canady <jon@joncanady.com>
2010-11-01 11:24:02 -0400

    Adding personal cusotmizations

---
+++ jonc.el
@@ -0,0 +1,40 @@
+(defvar org-my-archive-expiry-days 2
+  "The number of days after which a completed task should be auto-archived.
+This can be 0 for immediate, or a floating point value.")
+
+(defun org-my-archive-done-tasks ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((done-regexp
+           (concat "\\* \\(" (regexp-opt org-done-keywords) "\\) "))
+          (state-regexp
+           (concat "- State \"\\(" (regexp-opt org-done-keywords)
+                   "\\)\"\\s-*\\[\\([^]\n]+\\)\\]")))
+      (while (re-search-forward done-regexp nil t)
+        (let ((end (save-excursion
+                     (outline-next-heading)
+                     (point)))
+              begin)
+          (goto-char (line-beginning-position))
+          (setq begin (point))
+          (when (re-search-forward state-regexp end t)
+            (let* ((time-string (match-string 2))
+                   (when-closed (org-parse-time-string time-string)))
+              (if (>= (time-to-number-of-days
+                       (time-subtract (current-time)
+                                      (apply #'encode-time when-closed)))
+                      org-my-archive-expiry-days)
+                  (org-archive-subtree)))))))))
+
+(defalias 'archive-done-tasks 'org-my-archive-done-tasks)
+
+
+(setq org-remember-templates
+      '(("Innova" ?i "** TODO %? %^g\n %i\n " "~/todo/innova.org" "Inbox")
+        ))
+
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+(define-key global-map "\C-cr" 'org-remember)
