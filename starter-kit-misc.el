f27dd95162dd1daa8c2b3d95405a6837ab398915
Jon Canady <jon@joncanady.com>
2010-11-02 10:22:10 -0400

    Moving customizations into jonc file, adding agends commands

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -165,8 +165,8 @@
 
 ;; textmate minor mode is awesome
 ;; http://ozmm.org/posts/textmate_minor_mode.htm
-(require 'textmate)
-(textmate-mode)
+;; (require 'textmate)
+;; (textmate-mode)
 
 (eval-after-load 'mumamo
   '(eval-after-load 'zenburn
@@ -243,52 +243,6 @@ With arg copies and reinserts last line."
 
 (global-set-key (kbd "C-;") 'comment-uncomment-line-or-region)
 
-
-
-
-(require 'color-theme)
-
-(defun color-theme-rblue ()
-  "Color theme by hosiawak, created 2007-03-15."
-  (interactive)
-  (color-theme-install
-   '(color-theme-rblue
-     ((background-color . "#162433")
-      (background-mode . dark)
-      (border-color . "black")
-      (cursor-color . "yellow")
-      (foreground-color . "#C7D4E2")
-      (mouse-color . "sienna1"))
-     (default ((t (:background "black" :foreground "white"))))
-     (blue ((t (:foreground "blue"))))
-     (bold ((t (:bold t))))
-     (bold-italic ((t (:bold t))))
-     (border-glyph ((t (nil))))
-     (buffers-tab ((t (:background "black" :foreground "white"))))
-     (font-lock-builtin-face ((t (:foreground "white"))))
-     (font-lock-comment-face ((t (:foreground "#428BDD"))))
-     (font-lock-constant-face ((t (:foreground "#00CC00"))))
-     (font-lock-doc-string-face ((t (:foreground "DarkOrange"))))
-     (font-lock-function-name-face ((t (:foreground "white"))))
-     (font-lock-keyword-face ((t (:foreground "#F9BB00"))))
-     (font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
-     (font-lock-reference-face ((t (:foreground "SlateBlue"))))
-     (font-lock-string-face ((t (:foreground "#00CC00"))))
-     (font-lock-type-face ((t (:foreground "white"))))
-     (font-lock-variable-name-face ((t (:foreground "white"))))
-     (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
-     (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
-     (highlight ((t (:background "darkolivegreen"))))
-     (highline-face ((t (:background "SeaGreen"))))
-     (italic ((t (nil))))
-     (left-margin ((t (nil))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
-     (toolbar ((t (nil))))
-     (underline ((nil (:underline nil))))
-     (zmacs-region ((t (:background "snow" :foreground "ble")))))))
-
-(color-theme-rblue)
-(set-face-background 'hl-line "#330")
 
 
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
