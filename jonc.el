a3482e4d59090256fc7f7d01073158d1add81241
Jon Canady <jon@joncanady.com>
2011-01-05 09:14:14 -0500

    Comment out color-theme stuff in favor of load-theme (Emacs 24)

--- jonc.el
+++ jonc.el
@@ -42,58 +42,54 @@ This can be 0 for immediate, or a floating point value.")
 
 
 
-(require 'color-theme)
-(defun color-theme-rblue ()
-  "Color theme by hosiawak, created 2007-03-15."
-  (interactive)
-  (color-theme-install
-   '(color-theme-rblue
-     ((background-color . "#162433")
-      (background-mode . dark)
-      (border-color . "black")
-      (cursor-color . "yellow")
-      (foreground-color . "#C7D4E2")
-      (mouse-color . "sienna1"))
-     (default ((t (:background "black" :foreground "white"))))
-     (blue ((t (:foreground "blue"))))
-     (bold ((t (:bold t))))
-     (bold-italic ((t (:bold t))))
-     (border-glyph ((t (nil))))
-     (buffers-tab ((t (:background "black" :foreground "white"))))
-     (font-lock-builtin-face ((t (:foreground "white"))))
-     (font-lock-comment-face ((t (:foreground "#428BDD"))))
-     (font-lock-constant-face ((t (:foreground "#00CC00"))))
-     (font-lock-doc-string-face ((t (:foreground "DarkOrange"))))
-     (font-lock-function-name-face ((t (:foreground "white"))))
-     (font-lock-keyword-face ((t (:foreground "#F9BB00"))))
-     (font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
-     (font-lock-reference-face ((t (:foreground "SlateBlue"))))
-     (font-lock-string-face ((t (:foreground "#00CC00"))))
-     (font-lock-type-face ((t (:foreground "white"))))
-     (font-lock-variable-name-face ((t (:foreground "white"))))
-     (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
-     (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
-     (highlight ((t (:background "darkolivegreen"))))
-     (highline-face ((t (:background "SeaGreen"))))
-     (italic ((t (nil))))
-     (left-margin ((t (nil))))
-     (text-cursor ((t (:background "yellow" :foreground "black"))))
-     (toolbar ((t (nil))))
-     (underline ((nil (:underline nil))))
-     (zmacs-region ((t (:background "snow" :foreground "ble")))))))
+;; (require 'color-theme)
+;; (defun color-theme-rblue ()
+;;   "Color theme by hosiawak, created 2007-03-15."
+;;   (interactive)
+;;   (color-theme-install
+;;    '(color-theme-rblue
+;;      ((background-color . "#162433")
+;;       (background-mode . dark)
+;;       (border-color . "black")
+;;       (cursor-color . "yellow")
+;;       (foreground-color . "#C7D4E2")
+;;       (mouse-color . "sienna1"))
+;;      (default ((t (:background "black" :foreground "white"))))
+;;      (blue ((t (:foreground "blue"))))
+;;      (bold ((t (:bold t))))
+;;      (bold-italic ((t (:bold t))))
+;;      (border-glyph ((t (nil))))
+;;      (buffers-tab ((t (:background "black" :foreground "white"))))
+;;      (font-lock-builtin-face ((t (:foreground "white"))))
+;;      (font-lock-comment-face ((t (:foreground "#428BDD"))))
+;;      (font-lock-constant-face ((t (:foreground "#00CC00"))))
+;;      (font-lock-doc-string-face ((t (:foreground "DarkOrange"))))
+;;      (font-lock-function-name-face ((t (:foreground "white"))))
+;;      (font-lock-keyword-face ((t (:foreground "#F9BB00"))))
+;;      (font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
+;;      (font-lock-reference-face ((t (:foreground "SlateBlue"))))
+;;      (font-lock-string-face ((t (:foreground "#00CC00"))))
+;;      (font-lock-type-face ((t (:foreground "white"))))
+;;      (font-lock-variable-name-face ((t (:foreground "white"))))
+;;      (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
+;;      (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
+;;      (highlight ((t (:background "darkolivegreen"))))
+;;      (highline-face ((t (:background "SeaGreen"))))
+;;      (italic ((t (nil))))
+;;      (left-margin ((t (nil))))
+;;      (text-cursor ((t (:background "yellow" :foreground "black"))))
+;;      (toolbar ((t (nil))))
+;;      (underline ((nil (:underline nil))))
+;;      (zmacs-region ((t (:background "snow" :foreground "ble")))))))
 
 ;; (color-theme-rblue)
 ;; (set-face-background 'hl-line "#330")
-(require 'color-theme-tangotango)
-(color-theme-tangotango)
-
-(setq org-agenda-custom-commands
-      `(("d" agenda "Today"
-     ((org-agenda-filter-preset '("+today")))
-     )))
+;; (require 'color-theme-tangotango)
+;; (color-theme-tangotango)
+(load-theme 'wheatgrass)
 
 (setq org-agenda-custom-commands 
-      '(("d" "Today" tags-todo "today")))
+      '(("d" "Things to do Today" tags-todo "today")))
 
 
 (setq org-agenda-files
@@ -103,3 +99,48 @@ This can be 0 for immediate, or a floating point value.")
 (server-start)
 
 
+(defvar org-my-archive-expiry-days 2
+  "The number of days after which a completed task should be auto-archived.
+This can be 0 for immediate, or a floating point value.")
+
+(defun org-my-archive-done-tasks ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((done-regexp
+           (concat "\\* \\(" (regexp-opt org-done-keywords) "\\) "))
+          (state-regexp
+           (concat "- State \"\\(" (regexp-opt org-done-keywords)
+                   "\\)\"\\s-*\\[\\([^]\n]+\\)\\]")))
+      (while (re-search-forward done-regexp nil t)
+        (let ((end (save-excursion
+                     (outline-next-heading)
+                     (point)))
+              begin)
+          (goto-char (line-beginning-position))
+          (setq begin (point))
+          (when (re-search-forward state-regexp end t)
+            (let* ((time-string (match-string 2))
+                   (when-closed (org-parse-time-string time-string)))
+              (if (>= (time-to-number-of-days
+                       (time-subtract (current-time)
+                                      (apply #'encode-time when-closed)))
+                      org-my-archive-expiry-days)
+                  (org-archive-subtree)))))))))
+
+(defalias 'archive-done-tasks 'org-my-archive-done-tasks)
+
+
+
+(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+                                     interpreter-mode-alist))
+
+(autoload 'mode-compile "mode-compile"
+  "Compile current buffer based on the major mode" t)
+(global-set-key (kbd "C-c c") 'mode-compile)
+(autoload 'mode-compile-kill "mode-compile"
+  "Kill compilation launched by `mode-compile'" t)
+
+(global-set-key (kbd "C-c k")'mode-compile-kill)
+
+(rvm-use-default) 
